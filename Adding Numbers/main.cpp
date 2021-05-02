a8e2815f72ab03b64a5757ea0c794a63767df8ea
Manvir Singh <manvirsinghak47@gmail.com>
2021-05-02 13:02:27 +0530

    add comment

--- Adding Numbers/main.cpp
+++ Adding Numbers/main.cpp
@@ -12,10 +12,16 @@ int main()
     int num2;  //will hold the second input
     int sum;   //will hold the sum
     
+    //getting value
     cout<<"Please enter two numbers seperated by a space:"<<endl;
+    
     cin>>num1>>num2;
     
+    //adding two numbers given by user
+    
     sum = num1 + num2;
+    
+    //print sum of two numbers
     
     cout<<num1<<" + "<<num2<<" = "<<sum<<endl;
     
